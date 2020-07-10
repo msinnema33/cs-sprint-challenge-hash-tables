e63caf5ed321a2fdcf4ad2a164fbf14aadc94d52
Mike Sinnema <msinnema33@gmail.com>
2020-07-10 08:42:07 -0700

    indices of weights (ex1) passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight = {}
+    dupe_check = False # in order to pass test #2
+    dupes = {} # we need to account for duplicat weights ie: [10,10]
+    #loop through weights and store them in the dict
+    for i in range(0, length):
+        current = weights[i]
+        weight[current] = i #set the value to the index
+        target = limit - current # we subtract the current value from the limit to find which package combines to equal weight limit
+        if target in weight:
+            if current > target or current < target:
+                return (i, weight[target])
+            elif target == current: # if it finds a dupe
+                if dupe_check is False:
+                    dupe_check = True
+                    dupes[current] = i
+                elif dupe_check is True:
+                    return (i, dupes[current])
 
     return None
