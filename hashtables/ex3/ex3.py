dc7f457449e8e9509e203f49882f109a5c645fd6
Mike Sinnema <msinnema33@gmail.com>
2020-07-10 09:04:03 -0700

    intersection of arrays (ex3) passing tests

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,9 +2,20 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
-    return result
+    store = dict()
+    res = []
+    for index, arr in enumerate(arrays): # list all of the elements of a set
+        for v in arr:
+            if store.get(v) is not None and index > 0: #.get returns value from storage
+                store[v] += 1 # we either add on to an existing value indicating it's found in more than one arr
+            elif store.get(v) is None and index == 0:
+                store[v] = 1 #or we start a new spot for the first arr's value
+            else:
+                continue # if a new num in a following arr that wasnt seen in previous arr, skip it as it cannot be in common
+    for values in store:
+        if store[values] == len(arrays):
+            res.append(values) # if val is present in all subarrays, add to result list
+    return res
 
 
 if __name__ == "__main__":
