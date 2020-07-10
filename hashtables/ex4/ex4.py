2bf8939ffeb90a2c1f04e8e3cd0d2e809d9133d6
Mike Sinnema <msinnema33@gmail.com>
2020-07-10 09:16:13 -0700

    has negatives (ex4) passing tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,9 +2,19 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # dict to hold all nums in a
+    store = {}
+    # arr to return matches in absolute value(result)
+    res = []
+    for num in a:
+        # if the num has a corresponding number in dict
+        if store.get(abs(num)): # .get method returns the value for the given key
+            res.append(abs(num)) # if so add to results list
+        else:
+            # if no val is found add num (the value) to the store
+            store[abs(num)] = num
 
-    return result
+    return res
 
 
 if __name__ == "__main__":
